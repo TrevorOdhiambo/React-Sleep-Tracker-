d9bc7f94a43cd2d71f89d4c187b368f78119df66
TrevorOdhiambo <trevodhiambo@gmail.com>
2021-12-07 11:41:11 +0300

    Created responsive navigation bar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,46 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import './navbar.css'
+import { FaBars,FaTimes} from 'react-icons/fa';
 const Navbar = ()=>{
+  
+  const [click,setClick] = useState(false)
+  const handleClick = ()=>{
+    setClick(!click)
+  }
+
   return(
-    <h1>Nav</h1>
+  
+     <nav className="navbar">
+       <div className='logo'> <a href="" className='logo-text'>Sleepy</a></div>
+       <ul className={click ? "nav-list active" : "nav-list"}>
+         <li className="nav-item">
+           <a href="#">How it works</a>
+         </li>
+         <li className="nav-item">
+           <a href="#">About</a>
+         </li>
+         <li className="nav-item">
+           <a href="#">Sleep Sounds</a>
+         </li>
+         <li className="nav-item">
+           <a href="#" >Sleep quiz</a>
+           <span className="border"></span>
+         </li>
+      
+         <li className="nav-item">
+           <a href="#" className='signup-btn'>Try it for free</a>
+         </li>
+       </ul>
+       <div className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <FaTimes className="menu-icon" />
+        ) : (
+          <FaBars className="menu-icon" />
+        )}
+      </div>
+     </nav>
+
   )
 }
 
-export default Navbar
+export default Navbar 
